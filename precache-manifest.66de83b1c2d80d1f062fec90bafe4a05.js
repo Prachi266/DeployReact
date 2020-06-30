c78b71842f6e4dfb737ac0b10d9ec9ea20851824
Prachi266 <prachiparihar.p26@gmail.com>
2020-06-30 16:31:03 +0530

    Updates

---
+++ precache-manifest.66de83b1c2d80d1f062fec90bafe4a05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53c22c783720ce8da72002e5dad4784",
+    "url": "/DeployReact/index.html"
+  },
+  {
+    "revision": "b8ac737d15758e18f162",
+    "url": "/DeployReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88dc64ce066893d8f762",
+    "url": "/DeployReact/static/js/2.dcf6d49d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DeployReact/static/js/2.dcf6d49d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ac737d15758e18f162",
+    "url": "/DeployReact/static/js/main.33c5a8d3.chunk.js"
+  },
+  {
+    "revision": "7ab7c8907dfe49b617cf",
+    "url": "/DeployReact/static/js/runtime-main.26e3ef36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DeployReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
